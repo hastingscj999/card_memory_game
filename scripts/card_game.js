fc3e77207a12f6631960407560378385bb0d8dfe
Chris Hastings <chastings@conversantmedia.com>
2017-05-16 18:05:18 -0500

    improved logic for automatic folding when match fails

--- scripts/card_game.js
+++ scripts/card_game.js
@@ -53,12 +53,38 @@ for (var j in cards){
 	cards[j].content.style.left = cardLeft;
 	cards[j].content.style.top = cardTop;
 
+	/* revert an array of cards back to inactive, face-down state */
+	function revertCards(els){
+		var n = els.length;	
+		setTimeout(function(){
+			while (n--){
+				els[n].innerHTML = "";
+				els[n].style.backgroundImage = "url(images/card-down.jpg)";
+				els[n].style.border = "";
+				els[n].classList.add("inactive");
+				els[n].classList.remove("active");
+			}
+		}, 250);
+	}
+
+	/* remove matched cards from view */
+	function removeCards(els){
+		var n = els.length;	
+		/* delay removal so that user briefly can see both matching face-up cards */
+		setTimeout(function(){
+			while (n--){
+				document.body.removeChild(els[n]);
+			}
+			
+		}, 250);		
+	}
+
 	/* manages card behavior */
 	cards[j].content.onclick = function(ev){
 		var el = ev.target;
-		/* detects that cards can still be turned and current card is inactive */
-		if (count < 2 && el.classList[1] === "inactive"){
-			/* iterate count */
+		/* condition for inactive card */
+		if (el.classList[1] === "inactive"){
+			/* iterate count if card not alread active */
 			count++;
 			/* add open card value to comparator array */
 			openCards.push(el.classList[0]);
@@ -69,45 +95,35 @@ for (var j in cards){
 			el.style.backgroundImage = "";
 			el.style.border = "solid 1px #152b4f";
 			el.innerHTML = "<p class='data' id=''>" + el.classList[0] + "</p>";
-			/* if comparator array has 2 equal values, remove open cards */
-			if (openCards[0] === openCards[1]){
-				function removeCards(){
+			if (count === 2){	
+				/* if comparator array has 2 equal values, remove open cards */
+				if (openCards[0] === openCards[1]){
 					var candidates = document.getElementsByClassName(el.classList[0]);
-					var k = candidates.length;
-					/* delay removal so that user briefly can see both matching face-up cards */
-					setTimeout(function(){
-						while (k--){
-							if (candidates[k].classList && candidates[k].classList[0] === openCards[0]){
-								document.body.removeChild(candidates[k]);
-							}	
-						}
-						/* reset open card values and iterator */
-						openCards = [];
-						count = 0;
-					}, 500);		
+					removeCards(candidates);
+					var els = document.getElementsByClassName("inactive");
+					/* display winning message if all cards have been removed */
+					if (els.length === 0){
+						var congrats = document.createElement("div");
+						congrats.id = "congrats";
+						congrats.innerHTML = "<div id='winner'>Congrats! You've won.</div>";
+						document.body.appendChild(congrats);					
+					}				
+				} else {
+					/* revert cards to face down after a delay to show open cards */
+					console.log("no match");	
+					var els = document.getElementsByClassName("active");
+					revertCards(els);		
 				}
-				removeCards();
-				var els = document.getElementsByClassName("inactive");
-				/* display winning message if all cards have been removed */
-				if (els.length === 0){
-					var congrats = document.createElement("div");
-					congrats.id = "congrats";
-					congrats.innerHTML = "<div id='winner'>Congrats! You've won.</div>";
-					document.body.appendChild(congrats);					
-				}				
-			} 
-		} else {
-			/* revert card to inactive, face-down state */
-			if (el.classList[1] === "active"){
-				count--;
-				var index = openCards.indexOf(el.classList[0]);
-				openCards.splice(index, 1);		
-				el.innerHTML = "";
-				el.style.backgroundImage = "url(images/card-down.jpg)";
-				el.style.border = "";
-				el.classList.add("inactive");
-				el.classList.remove("active");
+				/* reset open card values and iterator */
+				openCards = [];
+				count = 0;
 			}
+		} else {
+			/* revert card to inactive, face-down state if user has toggled active card */
+			count--;
+			var index = openCards.indexOf(el.classList[0]);
+			openCards.splice(index, 1);		
+			revertCards([el]);
 		}
 	};
 }
